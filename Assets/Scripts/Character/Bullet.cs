3b89ca7f34dbae625c290bd7f41ba4d7395a73eb
Ngoc Anh <ngocanh.a167@gmail.com>
2022-02-23 17:43:11 +0700

    fix collider/bullet death

--- Assets/Scripts/Character/Bullet.cs
+++ Assets/Scripts/Character/Bullet.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public GameObject prefabExplosion;
+    protected Timer deathTimer;
+    protected float lifeSeconds;
 
     // Start is called before the first frame update
     void Start()
     {
+        lifeSeconds = 5;
+        deathTimer = gameObject.AddComponent<Timer>();
+        deathTimer.Duration = lifeSeconds;
+        deathTimer.Run();
         GameObject spacecraft = GameObject.FindGameObjectWithTag("Player");
         GetComponent<Rigidbody2D>().AddForce(spacecraft.transform.up*10, ForceMode2D.Impulse);
     }
@@ -17,7 +23,11 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // kill bullet when timer is done
+        if (deathTimer.Finished)
+        {
+            Destroy(gameObject);
+        }
     }
     // Destroy if collide with creep
     void OnCollisionEnter2D(Collision2D collision)
