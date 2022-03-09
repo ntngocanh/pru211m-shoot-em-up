d70dac4576c7ed536d1eecb1a4236f99fe15ab0a
luongtran14 <luongtranduc2001@gmail.com>
2022-03-09 21:00:43 +0700

    change some prefabs

---
+++ Assets/Scripts/Util/Restart.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        Debug.Log("Restart called");
+        SceneManager.LoadScene("SampleScene"); // loads current scene
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
